6367dd6b24a759d3f7c4595a36edf242a4143439
isaac anaya <isaacanayavaldez@gmail.com>
2025-11-17 00:20:38 +0000

    SE CORTA ESTA OREJA de la parte de arriba (_for element svg_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,7 +5,7 @@ export const EarIcon = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
-    viewBox="0 0 24 24"
+    viewBox="0 2 24 24"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
