69d82d862f76031bbfb889fe86da70e83274e99d
isaac anaya <isaacanayavaldez@gmail.com>
2025-11-18 17:52:49 +0000

    SE SIGUE CORTANDO

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -5,7 +5,7 @@ export const EarIcon = (props: SVGProps<SVGSVGElement>) => (
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
-    viewBox="0 2 24 24"
+    viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
@@ -13,10 +13,11 @@ export const EarIcon = (props: SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M6 10a7 7 0 1 1 12 0v5a7 7 0 0 1-7 7h-1a7 7 0 0 1-5-7Z" />
-    <path d="M12 10a3 3 0 0 1 3 3v2" />
+    <path d="M6 8.5a6.5 6.5 0 1 1 13 0v6a6.5 6.5 0 0 1-6.5 6.5h-1A6.5 6.5 0 0 1 5 14.5v-6Z" />
+    <path d="M12 8.5a3 3 0 0 1 3 3v2" />
   </svg>
 );
+
 
 export const BonitoLogo = (props: SVGProps<SVGSVGElement>) => (
     <svg
