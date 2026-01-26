34091a89f623079b539be07b3ba00ac606df2963
isaac anaya <isaacanayavaldez@gmail.com>
2026-01-26 02:43:48 +0000

    quitemos por ahora el modulo de linkz a redes sociales del lado izquierd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { LanguageProvider } from '@/context/language-context';
-import SocialLinks from '@/components/social-links';
 
 export default function RootLayout({
   children,
@@ -19,7 +18,6 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         <LanguageProvider>
-          <SocialLinks />
           {children}
           <Toaster />
         </LanguageProvider>
